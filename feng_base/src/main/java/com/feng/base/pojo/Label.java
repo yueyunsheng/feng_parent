11cd2bedf39f65dd4728ff591a981ef9cb7ef561
UltimateGlory <270453244@qq.com>
2019-06-10 18:40:56 +0800

    测试一下哦

---
+++ feng_base/src/main/java/com/feng/base/pojo/Label.java
@@ -0,0 +1,59 @@
+package com.feng.base.pojo;
+
+public class Label {
+
+    private String id;
+    private String labelName;
+    private String state;
+    private Long count;
+    private Long fans;
+    private String recommend;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabelName() {
+        return labelName;
+    }
+
+    public void setLabelName(String labelName) {
+        this.labelName = labelName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public Long getFans() {
+        return fans;
+    }
+
+    public void setFans(Long fans) {
+        this.fans = fans;
+    }
+
+    public String getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(String recommend) {
+        this.recommend = recommend;
+    }
+}
